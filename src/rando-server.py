fae0a0137afc5f3f03c0ec9588ee890e5915f12d
Chris Rust <chris_rust@mckinsey.com>
2021-06-29 17:19:28 -0500

    Load views externally

--- src/rando-server.py
+++ src/rando-server.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from shared import bikecalc
 from models import db
 
+from views import users
+
+def load_modules(app=None):
+    users.init_app(app)
+
 def load_app():
     app = FastAPI(title="Rando Server")
     db.init_app(app)
+    load_modules(app)
     return app
 
 app = load_app()
@@ -25,27 +31,3 @@ async def calculate_gear_ratios(min_chainring: int = 36, max_chainring: int = 50
     Default values are from my bike."""
     return bikecalc.calculate_gear_ratios(min_chainring, max_chainring, min_cog, max_cog)
 
-
-
-from pydantic import BaseModel
-
-from models.users import User
-
-@app.get("/users/{uid}")
-async def get_user(uid: int):
-    user = await User.get_or_404(uid)
-    return user.to_dict()
-
-class UserModel(BaseModel):
-    name: str
-
-@app.post("/users")
-async def add_user(user: UserModel):
-    rv = await User.create(nickname=user.name)
-    return rv.to_dict()
-
-@app.delete("/users/{uid}")
-async def delete_user(uid: int):
-    user = await User.get_or_404(uid)
-    await user.delete()
-    return dict(id=uid)
