2aa64e902f9b9ddb8b28304cd4ca1e3e85a4fddc
Chris Rust <chris_rust@mckinsey.com>
2021-07-04 15:58:45 -0500

    Add initial alembic code, move server filename to underscore to support importing by alembic

--- src/rando_server.py
+++ src/rando_server.py
@@ -12,14 +12,18 @@ logger = logging.getLogger("uvicorn.error")
 #  gino.ext.starlette, uvicorn.error, gino.engine._SAEngine, etc..
 
 def load_modules(app=None):
+    # walk each module from views and import
     path = views.__path__
     prefix = views.__name__ + "."
     for importer, modname, ispkg in pkgutil.iter_modules(path, prefix):
         module = importer.find_module(modname).load_module(modname)
-        init_app = getattr(module, "init_app", None)
-        if init_app:
-            init_app(app)
-            logger.info(f"Imported routes from {modname} module.")
+        # load individual routes from each view into FastAPI app
+        # skip this process when app is None during alembic runs
+        if app:
+            init_app = getattr(module, "init_app", None)
+            if init_app:
+                init_app(app)
+                logger.info(f"Imported routes from {modname} module.")
 
 def load_app():
     app = FastAPI(title="Rando Server")
