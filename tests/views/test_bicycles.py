a49d03e172d461133010189e21447a6e20fc8240
Chris Rust <chris_rust@mckinsey.com>
2021-07-09 23:27:54 -0500

    Reword comments

--- tests/views/test_bicycles.py
+++ tests/views/test_bicycles.py
@@ -2,14 +2,14 @@ import uuid
 
 def test_crud(client):
     # create
-    # generate unique values for fields
-    year = uuid.uuid1().int % 10000 # 4 digit remainder of dividing by 10000
+    # generate unique values for bicycle fields
+    year = uuid.uuid1().int % 10000 # get the 4 digit remainder of division by 10000, to mock 4 digit year
     make = str(uuid.uuid4())
     model = str(uuid.uuid4())
     r = client.post("/bicycles", json=dict(year=year, make=make, model=model))
     r.raise_for_status()
 
-    # retrieve
+    # read
     url = f"/bicycles/{r.json()['id']}"
     r = client.get(url).json()
     assert r["year"] == year
